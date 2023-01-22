d03c0693fb866071e11bd849c738b6df588a3a97
Weronika Sawicka <weronikakowalczyk519@gmail.com>
2023-01-22 15:20:04 +0100

    Zadanie domowe dzień 5 podejście 1

--- ChallengeAppWerka21days/ChallengeAppWerka21days/Program.cs
+++ ChallengeAppWerka21days/ChallengeAppWerka21days/Program.cs
@@ -1,34 +1,32 @@
-﻿// zadanie domowe dzień 4 podejście 2
-using System;
+﻿// Zadanie domowe dzień 5 podejście 1
 
-string name1 = "Ewa";
-string name2 = "Weronika";
-string name3 = "Maksymilian";
+int number = 291294;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToCharArray();
 
-int age1 = 40;
-int age2 = 28;
-int age3 = 16;
+List<char> liczby = new List<char>();
+liczby.Add('0');
+liczby.Add('1');
+liczby.Add('2');
+liczby.Add('3');
+liczby.Add('4');
+liczby.Add('5');
+liczby.Add('6');
+liczby.Add('7');
+liczby.Add('8');
+liczby.Add('9');
 
-Char sex1 = 'K';
-Char sex2 = 'M';
 
-string result1 = name1 + age1 + sex1;
-string result2 = name2 + age2 + sex1;
-String result3 = name3 + age3 + sex2;
+Console.WriteLine("Liczba do analizy: " + number);
 
-// Console.WriteLine(result1);
-// Console.WriteLine(result2);
-// Console.WriteLine(result3);
-
-if (name1 == "Ewa" && age1 < 30)
+foreach (var singular in liczby)
 {
-    Console.WriteLine("Jest tu Ewa poniżej przed trzydziestką");
+    int count = 0;
+    foreach (var x in letters)
+    {
+        if (singular == x) count++;
+    }
+    Console.WriteLine(singular + "=>" + count);
 }
-else if (name2 == "Weronika" && age2 == 28)
-{
-    Console.WriteLine("Weronika ma 28 lat");
-}
-else if (sex2 == 'M' && name3 == "Robert" && age3 < 18)
-{
-    Console.WriteLine("Nie ma tu niepełnoletniego Roberta");
-}
+
+
