c414f6ec9208793178d139a4a406157636e03271
Weronika Sawicka <weronikakowalczyk519@gmail.com>
2023-01-24 22:01:54 +0100

    Zadanie domowe, dzień 6. Podejście 1.

---
+++ ChallengeAppWerka21days/ChallengeAppWerka21days/Employee.cs
@@ -0,0 +1,25 @@
+﻿class Employee
+{
+    private List<int> score = new List<int>();
+    public Employee(string name, string surname, int age)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.Age = age;
+    }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+    public int Result
+    {
+        get
+        {
+            return this.score.Sum();
+        }
+    }
+
+    public void AddScore(int number)
+    {
+        this.score.Add(number);
+    }
+}
--- ChallengeAppWerka21days/ChallengeAppWerka21days/Program.cs
+++ ChallengeAppWerka21days/ChallengeAppWerka21days/Program.cs
@@ -1,32 +1,36 @@
-﻿// Zadanie domowe dzień 5 podejście 1
+﻿using System;
+using System.Reflection.Metadata;
 
-int number = 291294;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToCharArray();
+Employee employee1 = new Employee("Anna", "Cieszkowska", 36);
+Employee employee2 = new Employee("Hanna", "Szczecińska", 40);
+Employee employee3 = new Employee("Norbert", "Leszkoń", 55);
 
-List<char> liczby = new List<char>();
-liczby.Add('0');
-liczby.Add('1');
-liczby.Add('2');
-liczby.Add('3');
-liczby.Add('4');
-liczby.Add('5');
-liczby.Add('6');
-liczby.Add('7');
-liczby.Add('8');
-liczby.Add('9');
+employee1.AddScore(3);
+employee1.AddScore(5);
+employee1.AddScore(8);
+
+employee2.AddScore(10);
+employee2.AddScore(5);
+employee2.AddScore(2);
+
+employee3.AddScore(6);
+employee3.AddScore(7);
+employee3.AddScore(8);
+
+var Result1 = employee1.Result;
+var Result2 = employee2.Result;
+var Result3 = employee3.Result;
 
 
-Console.WriteLine("Liczba do analizy: " + number);
-
-foreach (var singular in liczby)
+if (employee1.Result >= employee2.Result && employee1.Result >= employee3.Result)
 {
-    int count = 0;
-    foreach (var x in letters)
-    {
-        if (singular == x) count++;
-    }
-    Console.WriteLine(singular + "=>" + count);
+    Console.WriteLine($"Pracownikiem miesiąca zostaje: \n {employee1.Name} \n {employee1.Surname} \n Lat: {employee1.Age} \n osiągnął on wynik {employee1.Result} punktów");
 }
-
-
+else if (employee2.Result >= employee1.Result && employee2.Result >= employee3.Result)
+{
+    Console.WriteLine($"Pracownikiem miesiąca zostaje: \n {employee2.Name} \n {employee2.Surname} \n Lat: {employee2.Age} \n osiągnął on wynik {employee2.Result} punktów");
+}
+else if (employee3.Result >= employee1.Result && employee3.Result >= employee2.Result)
+{
+    Console.WriteLine($"Pracownikiem miesiąca zostaje: \n {employee3.Name} \n {employee3.Surname} \n Lat: {employee3.Age} \n osiągnął on wynik {employee3.Result} punktów");
+}
